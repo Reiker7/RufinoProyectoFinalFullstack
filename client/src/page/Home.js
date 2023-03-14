c57a3a806c4dd7a29be954f2a6104effe9efd17f
Reiker <reiker1@hotmail.com>
2023-03-14 22:36:11 +0100

    awaken

--- client/src/page/Home.js
+++ client/src/page/Home.js
@@ -2,6 +2,9 @@ import { margin } from "@mui/system";
 import React, { useState, useEffect } from "react";
 import swain from "../assets/images/swainB.jpg";
 function Prueba() {
+  let awakendb
+  fetch('https://projectrufinodb.onrender.com/api/favorites')
+  .then(awakendb => console.log(awakendb.status))
   return (
     <div className="main">
       <img
